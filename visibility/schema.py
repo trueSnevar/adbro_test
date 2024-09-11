d5040976c2d6bf9d6a6f045317cc5c52fb5090f4
Aleksey Knyazev <aknyazev87@gmail.com>
2024-09-11 11:40:05 +0700

    Added extra model to populate queries with additional data + Migration

--- visibility/schema.py
+++ visibility/schema.py
@@ -1,7 +1,10 @@
+import graphene
+
 from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
-import graphene
 from graphql import GraphQLError
+
+from .models import Item
 
 User = get_user_model()
 
@@ -11,19 +14,48 @@ class UserType(DjangoObjectType):
         model = User
         fields = ('id', 'username', 'email')
 
+class ItemType(DjangoObjectType):
+    class Meta:
+        model = Item
+        fields = ('id', 'name', 'description', 'price')
 
-class UserByIdQuery(graphene.ObjectType):
-    user = graphene.Field(UserType, id=graphene.Int(required=True), description="Gets single User by ID")
+
+class Query(graphene.ObjectType):
+    user = graphene.Field(
+        UserType,
+        id=graphene.Int(required=True),
+        description="Gets single User by ID"
+    )
+    item = graphene.Field(
+        ItemType,
+        id=graphene.Int(required=True),
+        description="Get a single item by ID"
+    )
+    all_items = graphene.List(
+        ItemType,
+        description="Get all items"
+    )
 
     def resolve_user(self, info, id):
         user = info.context.user
         if not user.has_perm('auth.view_user'):
-            raise GraphQLError(f"Cannot query field `{info.field_name}` on type `{info.parent_type}`.")
+            raise GraphQLError(
+                f"Cannot query field `{info.field_name}` on type `{info.parent_type}`."
+            )
 
         try:
             return User.objects.get(id=id)
         except User.DoesNotExist:
             raise GraphQLError(f"User with id={id} not found.")
 
+    def resolve_item(self, info, id):
+        try:
+            return Item.objects.get(id=id)
+        except Item.DoesNotExist:
+            return None
 
-schema = graphene.Schema(query=UserByIdQuery)
+    def resolve_all_items(self, info):
+        return Item.objects.all()
+
+
+schema = graphene.Schema(query=Query)
